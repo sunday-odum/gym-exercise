0726a8db84481d5dbf463970f9b9b214259426a9
sunday odum <sundayodum@gmail.com>
2022-07-18 00:14:07 +0100

    page and commnet setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
-
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-       hello world
-    </div>
+    <Box width="400px" sx={{ width: {xl: '1448px'}}} m="auto">
+      <Navbar/>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/exercise/:id" element={<ExerciseDetail/>} />
+      </Routes>
+      <Footer/>
+    </Box>
   );
 }
 
